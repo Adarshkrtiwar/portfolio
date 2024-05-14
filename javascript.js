4f8ecf5e870326132f7e1402b3d7c6381d6b90f9
Adarshkrtiwar <97175973+Adarshkrtiwar@users.noreply.github.com>
2024-05-14 11:21:32 +0530

    Add files via upload
    Portfolio code

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#hero-text', {
+    strings: ['Software Engineer', 'Problem Solver', 'Creative Thinker','Full Stack Developer', 'Machine Learning Enthusiast', 'Open Source Contributor'],
+    typeSpeed: 50,
+    loop: true
+  });
+  
